41eacf5c5c7f2809aa1b52ffc9bbf89dac71e47f
Emil Helgesson <helgesson.emil.94@gmail.com>
2022-11-20 15:59:14 +0100

    added alternate scenes and an ending scene

--- main.js
+++ main.js
@@ -11,17 +11,15 @@ function loadFirstScene() {
 
     div.classList = "container";
     p.classList = "text-style";
-    button1.classList ="button1-style";
+    button1.classList ="button-style";
 
     p.textContent = "You are a young bounty hunter who has been falsely sentenced to spend the rest of your life in prison for a crime that you did not commit. One night you are awakened in your cell by a strange voice calling out from the other side of the prison bars.";
     button1.textContent = "Continue";
     button1.onclick = loadSecondScene;
-
     document.body.innerHTML = "";
     document.body.append(div);
     div.appendChild(p);
     div.appendChild(button1);
-    
 }
 
 function loadSecondScene() {
@@ -31,9 +29,9 @@ function loadSecondScene() {
     const button1 = document.createElement("button1");
 
     div.classList = "container";
-    imgDiv.classList = "imageSecondScene";
+    imgDiv.classList = "imgHoodedStranger";
     p.classList = "text-style";
-    button1.classList ="button1-style";
+    button1.classList ="button-style";
 
     p.textContent = "As you get closer you spot a mysterious hooded man gesturing for you to come closer...";
     button1.textContent = "Continue";
@@ -53,17 +51,21 @@ function loadThirdScene() {
     const speakerName = document.createElement("p");
     const p = document.createElement("p");
     const button1 = document.createElement("button1");
+    const button2 = document.createElement("button2");
 
     div.classList = "container";
-    imgDiv.classList = "imageSecondScene";
+    imgDiv.classList = "imgHoodedStranger";
     speakerName.classList = "speaker-name"
     p.classList = "text-style";
-    button1.classList ="button1-style";
+    button1.classList ="button-style";
+    button2.classList ="button-style";
 
     speakerName.textContent = "Hooded stranger"
-    p.textContent = "Well met prisoner, I have heard about your sentencing and how you deem it to be unjust. I do not have the authority to assist you to abolish you for your crimes. however, I can still offer you a way out of here on the condition that you provide me with your services as a bounty hunter";
+    p.textContent = "Well met prisoner, I have heard about your sentencing and how you deem it to be unjust. I do not have the authority to assist you to abolish your crimes. however, I can still offer you a way out of here on the condition that you provide me with your services as a bounty hunter";
     button1.textContent = "Continue";
     button1.onclick = loadFourthScene;
+    button2.textContent = "Decline his offer";
+    button2.onclick = loadDeclinedOfferScene;
 
     document.body.innerHTML = "";
     document.body.append(div);
@@ -71,7 +73,52 @@ function loadThirdScene() {
     div.appendChild(speakerName);
     div.appendChild(p);
     div.appendChild(button1);
-    
+    div.appendChild(button2);   
+}
+
+function loadDeclinedOfferScene() {
+    const div = document.createElement("div");
+    const imgDiv = document.createElement("div");
+    const speakerName = document.createElement("p");
+    const p = document.createElement("p");
+    const button1 = document.createElement("button1");
+
+    div.classList = "container";
+    imgDiv.classList = "imgHoodedStranger";
+    speakerName.classList = "speaker-name"
+    p.classList = "text-style";
+    button1.classList ="button-style";
+
+    speakerName.textContent = "Hooded stranger"
+    p.textContent = "Bahh...very well then! Enjoy spending the rest of your days in this dreadful cell";
+    button1.textContent = "Continue";
+    button1.onclick = loadEndingScene;
+
+    document.body.innerHTML = "";
+    document.body.append(div);
+    div.appendChild(imgDiv);
+    div.appendChild(speakerName);
+    div.appendChild(p);
+    div.appendChild(button1);   
+}
+
+function loadEndingScene() {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const button1 = document.createElement("button1");
+
+    div.classList = "container";
+    p.classList = "text-style-end";
+    button1.classList ="button-style";
+
+    p.textContent = "GAME OVER";
+    button1.textContent = "Try again";
+    button1.onclick = loadFirstScene;
+
+    document.body.innerHTML = "";
+    document.body.append(div);
+    div.appendChild(p);
+    div.appendChild(button1);
 }
 
 function loadFourthScene() {
@@ -80,17 +127,20 @@ function loadFourthScene() {
     const speakerName = document.createElement("p");
     const p = document.createElement("p");
     const button1 = document.createElement("button1");
+    const button2 = document.createElement("button2");
 
     div.classList = "container";
-    imgDiv.classList = "imageSecondScene";
+    imgDiv.classList = "imgHoodedStranger";
     speakerName.classList = "speaker-name"
     p.classList = "text-style";
-    button1.classList ="button1-style";
+    button1.classList ="button-style";
+    button2.classList ="button-style";
 
     speakerName.textContent = "Hooded stranger"
     p.textContent = "";
     button1.textContent = "Continue";
     button1.onclick = loadFourthScene;
+    button2.textContent = "Decline his offer";
 
     document.body.innerHTML = "";
     document.body.append(div);
@@ -98,5 +148,6 @@ function loadFourthScene() {
     div.appendChild(speakerName);
     div.appendChild(p);
     div.appendChild(button1);
+    div.appendChild(button2);
     
 }
